80a11905ce82a51cbad6528eb39fd01327accb7f
kylemiller014 <kylemiller014@gmail.com>
2024-05-09 22:43:14 -0500

    Initial package install...

--- app.R
+++ app.R
@@ -1,1 +1,15 @@
 # App Logic 
+library(shiny)
+library(shinymanager)
+library(plotly)
+library(ggplot2)
+# source("ui.R")
+# source("server.R")
+
+ui <- fluidPage(
+  "Hello, world!"
+)
+server <- function(input, output, session) {
+}
+
+shinyApp(ui, server)
