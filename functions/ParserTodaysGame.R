66593a792e2a388403484f43907a7435de3387e8
kylemiller014 <kylemiller014@gmail.com>
2024-07-26 12:43:48 -0500

    Create parser function for live games

--- functions/ParserTodaysGame.R
+++ functions/ParserTodaysGame.R
@@ -2,29 +2,52 @@
 # Required Libraries
 library(httr)
 library(jsonlite)
+library(tibble)
+library(purrr)
+library(stringr)
+library(dplyr)
+library(tidyr)
 
 # Define function
-# ParserTodayGame <- function() {}
+ParserTodayGame <- function() {
+  # Initial GET Call to Today's Game API
+  res <- GET("http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1")
+  
+  # Create raw datafram
+  data <- fromJSON(rawToChar(res$content), flatten = TRUE)
+  
+  # Convert raw data frame to unlisted tibble
+  dataRaw <- enframe(unlist(data))
+  
+  # Replace . with _
+  rgxDelim <- "\\."
+  dataRaw$name <- str_replace_all(dataRaw$name, rgxDelim, "_")
+  
+  # Create list of unique columns
+  # Remove digits
+  cleanedNames <- str_replace_all(dataRaw$name, "[:digit:]", "")
+  
+  # Unique column names
+  uniqueColumns <- unique(cleanedNames)
+  
+  # Columns for specific game information
+  todaysGamesColumns <- uniqueColumns[11:65]
+  
+  # Determine number of games happening today
+  numGames <- dataRaw[dataRaw$name == "totalItems",]
 
-res = GET("http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1")
-rawToChar(res$content)
-data = fromJSON(rawToChar(res$content), flatten = TRUE)
-
-names(data)
-data$totalItems
-data$dates$games
-
-gamesDF <- data.frame(data$dates$games)
-names(gamesDF)
-teamsDf <- gamesDF$teams.away.score
-gamesDF$teams
-
-finalDf <- teamsDf[,1:2]
-colnames(teamsDf)[c(1,2,3,4,6)] <- c("AWAY_TEAM_WINS", "AWAY_TEAM_LOSSES","AWAY_TEAM_PCT",
-                               "AWAY_SCORE", "AWAY_NAME")
-names(teamsDf)
-#,"HOME_TEAM_WINS", "HOME_TEAM_LOSSES","HOME_TEAM_PCT","HOME_SCORE", "HOME_NAME"
-awayTeam <- teamsDf[,1]
-teamsDf[1,1]
-select(teamsDf,away$leagueRecord$wins)
-fixed <- teamsDf[,c(1,2)]
+  # Loop through each column name
+  # Create a new dataframe with column names
+  parsedTodaysGames <- data.frame(matrix(nrow = as.integer(numGames$value), ncol = length(todaysGamesColumns)))
+  colnames(parsedTodaysGames) <- todaysGamesColumns
+  
+  for(i in 1:length(todaysGamesColumns)){
+    values <- dataRaw %>%
+      filter(grepl(todaysGamesColumns[i], dataRaw$name))
+    if (length(values$value) > as.integer(numGames$value)){
+      values <- values[1:as.integer(numGames$value),]
+    }
+    parsedTodaysGames[,i] <- values$value
+  }
+  return(parsedTodaysGames)
+}
